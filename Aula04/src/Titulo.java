7be2873a3a971f4064d6ff54657f384a7a61aaa7
Julia Karol <juliakarolinny044@gmail.com>
2026-08-05 16:34:11 -0300

    alterações
    mudei o nome de alguns livros.

--- Aula04/src/Titulo.java
+++ Aula04/src/Titulo.java
@@ -1,20 +1,20 @@
 public class Titulo {
     public String Twilight;;
-    public String  amoresimprovaveis;
-    public String crepusculo;
+    public String  Duna;
+    public String It;
 
-    public Titulo(String twilight, String amoresimprovaveis, String crepusculo) {
+    public Titulo(String twilight, String Duna, String It) {
         Twilight = twilight;
-        this.amoresimprovaveis = amoresimprovaveis;
-        this.crepusculo = crepusculo;
+        this.Duna = Duna;
+        this.It =It;
     }
 
     @Override
     public String toString() {
         return "Titulo{" +
                 "Twilight='" + Twilight + '\'' +
-                ", amoresimprovaveis='" + amoresimprovaveis + '\'' +
-                ", crepusculo='" + crepusculo + '\'' +
+                ", Duna='" + Duna + '\'' +
+                ",It='" + It + '\'' +
                 '}';
     }
 }
